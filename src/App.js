1f39ef1730bb548dc86d7dd9f7228ea029794f05
Felipe Emílio <felipeemilio27@gmail.com>
2024-12-11 16:08:10 -0300

    Criando Formulario

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import Banner from './componentes/Banner/index';
-import CampoTexto from './componentes/CampoText/index';
+import Formulario from './componentes/Formulario';
 
 function App() {
 
   return (
     <div className="App">
       <Banner /> 
-      <CampoTexto label = "Nome" placeholder = "Digite o seu nome"/>
-      <CampoTexto label = "Cargo" placeholder = "Digite o seu cargo"/>
-      <CampoTexto label = "Imagem" placeholder = "Digite o endereço da imagem"/>
+      <Formulario />
     </div>
   );
 
---
+++ src/componentes/Formulario/Formulario.js
@@ -0,0 +1,20 @@
+import CampoTexto from '../CampoText'
+import './Formulario.css'
+
+export const Formulario = () => {
+
+    return(
+
+        <section className="formulario">
+            <form>
+                <h2>Preencha os dados para criar o card do colaborador:</h2>
+                <CampoTexto label = "Nome" placeholder = "Digite o seu nome"/>
+                <CampoTexto label = "Cargo" placeholder = "Digite o seu cargo"/>
+                <CampoTexto label = "Imagem" placeholder = "Digite o endereço da imagem"/>
+            </form>
+        </section>
+
+    )
+
+}
+
