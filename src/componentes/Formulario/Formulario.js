a3cc8e19b49a825c64978401a925a0009d94e274
Felipe Emílio <felipeemilio27@gmail.com>
2024-12-12 13:51:35 -0300

    Lista dos Times

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -1,19 +1,27 @@
 import CampoTexto from '../CampoText'
+import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
 export const Formulario = () => {
 
-    return(
+    const Times = [
+        'Front-end',
+        'Back-end',
+        'Data Base',
+        'Redes',
+        'Devops'
+    ]
 
+    return(
         <section className="formulario">
             <form>
                 <h2>Preencha os dados para criar o card do colaborador:</h2>
                 <CampoTexto label = "Nome" placeholder = "Digite o seu nome"/>
                 <CampoTexto label = "Cargo" placeholder = "Digite o seu cargo"/>
                 <CampoTexto label = "Imagem" placeholder = "Digite o endereço da imagem"/>
+                <ListaSuspensa label = "Time" itens={Times}/>
             </form>
         </section>
-
     )
 
 }
