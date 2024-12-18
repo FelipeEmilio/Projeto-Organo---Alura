63c9a2933050f0a6895bfa2b739e3ac99d565075
Felipe Emílio <felipeemilio27@gmail.com>
2024-12-18 12:32:40 -0300

    Escondendo times vazios

--- src/componentes/Time/Time.js
+++ src/componentes/Time/Time.js
@@ -6,12 +6,13 @@ export const Time = ({nome, corPrimaria ,corSecundaria, colaboradores}) => {
     const css1 = { backgroundColor: corSecundaria }
     const css2 = { borderColor: corPrimaria }
     return (
-        <section className='time' style={css1}>
+       (colaboradores.length > 0) ? <section className='time' style={css1}>
             <h3 style={css2}>{nome}</h3>
             <div className='p'>
                 {colaboradores.map( pessoa => <Pessoas nome={pessoa.nome} cargo={pessoa.cargo} imagem={pessoa.imagem}/> )}
             </div>
         </section>
+        : ''
     )
 
 }
