e840e71ab534169c96e47c7a7373f9da854b0ce9
Felipe Emílio <felipeemilio27@gmail.com>
2024-12-18 12:58:26 -0300

    Criando Rodape

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Banner from './componentes/Banner/index';
 import Formulario from './componentes/Formulario';
 import Time from './componentes/Time/index';
+import Rodape from './componentes/Rodape';
 
 
 function App() {
@@ -66,6 +67,8 @@ function App() {
         corSecundaria={time.corSecundaria}
         colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
       />)}
+
+      <Rodape />
 
     </div>
   );
